fb442f1379c5b6358aab52feefbd4be4f2c82ec6
Badri Manishankar <badri.manishankar99@gmail.com>
2025-05-03 11:29:27 -0400

    Updating refresh rate to 60fps

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    fps = pygame.time.Clock()
+    dt = 0
 
     while True:
 
@@ -18,6 +20,7 @@ def main():
                 return
         screen.fill(color="black")
         pygame.display.flip()
+        fps.tick(60)
 
 
 if __name__ == "__main__":

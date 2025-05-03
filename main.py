67fc9e570e265108f8ff9b633db57f8c45d2d14e
Badri Manishankar <badri.manishankar99@gmail.com>
2025-05-03 14:14:05 -0400

    Add bullet collision behavior

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pygame
 import sys
+import pygame
 from constants import *
 from player import Player
 from asteroid import Asteroid
@@ -11,32 +11,45 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
     shots = pygame.sprite.Group()
+
     Asteroid.containers = (asteroids, updatable, drawable)
-    Player.containers = (updatable, drawable)
-    AsteroidField.containers = updatable
     Shot.containers = (shots, updatable, drawable)
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    AsteroidField.containers = updatable
     asteroid_field = AsteroidField()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         updatable.update(dt)
 
         for asteroid in asteroids:
-            if asteroid.check_collision(player):
+            if asteroid.collides_with(player):
                 print("Game over!")
                 sys.exit()
 
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    shot.kill()
+                    asteroid.split()
+
         screen.fill("black")
-        for draw in drawable:
-            draw.draw(screen)
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
 
         # limit the framerate to 60 FPS
